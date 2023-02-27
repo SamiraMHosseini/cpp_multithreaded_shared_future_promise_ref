939eb3d9a7b288562437ba89e8344b47a0e534e3
SamiraMHosseini <62559858+SamiraMHosseini@users.noreply.github.com>
2023-02-27 12:26:12 -0600

    Refactor the code

--- SandBox/main.cpp
+++ SandBox/main.cpp
@@ -27,20 +27,19 @@ int main()
 {
 	std::promise<int> prms;
 	std::promise<int> prms2;
-	std::shared_future<int> fut1 = prms.get_future().share();
+	std::shared_future<int> fut = prms.get_future().share();
 
-	std::future<ULL> fut1_result = std::async(std::launch::async, Fact, std::ref(fut1));
-	prms.set_value(3);
+	std::future<ULL> fut_result = std::async(std::launch::async, Fact, std::ref(fut));
+	prms.set_value(5);
 
-	std::cout << fut1_result.get() << '\n'; //It is waiting for a value to be returned by the Fact function.
+	std::cout << fut_result.get() << '\n'; //It is waiting for a value to be returned by the Fact function.
 
-	fut1 = prms2.get_future().share();
-	std::future<ULL> fut2_result = std::async(std::launch::async, Fact, std::ref(fut1));
-	prms2.set_value(4);
+	fut = prms2.get_future().share();
+	std::future<ULL> fut2_result = std::async(std::launch::async, Fact, std::ref(fut));
+	prms2.set_value(3);
 
 	std::cout << fut2_result.get() << '\n'; //It is waiting for a value to be returned by the Fact function.
 
-
-
+	return 0;
 
 }
